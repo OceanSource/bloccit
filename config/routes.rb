300785419bdefbd4dd62e428e7bb7f52bb364f7a
Ocean Source <oceansource@gmail.com>
2015-11-15 05:36:01 +0000

    Assignment-34 Topics and Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
  Rails.application.routes.draw do
 
-   resources :topics do
+  get 'sponsored_posts/show'
+
+  get 'sponsored_posts/new'
+
+  get 'sponsored_posts/edit'
+
+  get 'sponsored_post/show'
+
+  get 'sponsored_post/new'
+
+  get 'sponsored_post/edit'
+
+    resources :topics do
     resources :posts, except: [:index]
+    resources :sponsored_posts, except: [:index]
    end
 
    get 'about' => 'welcome#about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
  end
  topics = Topic.all
  
+
  # Create Posts
  50.times do
    Post.create!(
@@ -27,9 +28,25 @@
    )
  end
  
+   # Create SponsoredPosts
+ 50.times do
+   SponsoredPost.create!(
+     topic:  topics.sample,
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+ sponsored_posts = SponsoredPost.all
+ 
+
+ 
  puts "#{Post.count}"
  Post.find_or_create_by(title: "Unique title", body: "Unique body")
  puts "#{Post.count}"
+ 
+  puts "#{SponsoredPost.count}"
+ SponsoredPost.find_or_create_by(title: "Unique title", body: "Unique body")
+ puts "#{SponsoredPost.count}"
   
  puts "#{Comment.count}"
  Comment.find_or_create_by(body: "Unique body")
@@ -39,4 +56,5 @@
  puts "Seed finished"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
+ puts "#{SponsoredPost.count} sponsored posts created"
  puts "#{Comment.count} comments created"
