69af4bfffd257e06ed22fd8bf5f098e1030286d4
Ocean Source <oceansource@gmail.com>
2015-11-22 23:09:15 +0000

    assginment 33 added seed questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,8 @@
  posts = Post.all
  
  # Create Comments
- # #3
  100.times do
    Comment.create!(
- # #4
      post: posts.sample,
      body: RandomData.random_paragraph
    )
@@ -30,6 +28,20 @@
  puts "#{Comment.count}" 
   
   
+  # Create Questions
+ 50.times do
+ # #1
+   Question.create!(
+ # #2
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+ questions = Question.all 
+  
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Question.count} questions created"
+ 
+ 
