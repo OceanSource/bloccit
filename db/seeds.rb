845d59040871a44c8e073dbb1ed508346d4b1e2b
Ocean Source <oceansource@gmail.com>
2015-11-23 02:30:23 +0000

    checkpoint-40

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,9 +47,18 @@ include RandomData
  Comment.find_or_create_by(body: "Unique body")
  puts "#{Comment.count}" 
  
- user = User.first
- user.update_attributes!(
-   email: 'oceansource@gmail.com', # replace this with your personal email
+  # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
   
