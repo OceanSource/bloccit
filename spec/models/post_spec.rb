ac4d42efe7f131904114b25869ae54508ea8a587
Ocean Source <oceansource@gmail.com>
2015-12-03 01:45:28 +0000

    CHeckpoint-42

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,6 +5,9 @@
    let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
    let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
    let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
+   
+   it { should have_many(:labelings) }
+   it { should have_many(:labels).through(:labelings) }
 
    it { should have_many(:comments) }
    it { should belong_to(:topic) }
